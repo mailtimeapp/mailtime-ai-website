5d449c53f72329138d9e6bfee9d7347fffdfe8d0
fontofonto <donlam1994@gmail.com>
2023-05-04 01:05:19 -0700

    update the style of MailchimpSignupForm

--- src/components/MailchimpSignupForm.js
+++ src/components/MailchimpSignupForm.js
@@ -47,52 +47,49 @@ const CustomForm = ({ status, message, onValidated }) => {
       className="w-full lg:w-10/12 mx-auto flex flex-col justify-center px-[3.5rem] lg:px-0 items-center mt-[4.8rem] mb-48 lg:mb-72"
     >
       <div
-        className={`lg:w-full flex flex-col lg:flex-row ${
-          status === "sending" || status === "success"
-            ? "items-center"
-            : "items-center lg:items-start"
-        }`}
+        className={`mx-auto lg:w-full flex flex-col lg:flex-row justify-center space-y-[1.6rem] lg:space-y-0 lg:space-x-[1.6rem]`}
       >
+        {(status === null || status === "error") && (
+          <div className="w-full flex flex-col">
+            <input
+              aria-label="Your email"
+              ref={(node) => (emailInputElement = node)}
+              type="email"
+              placeholder="Your email"
+              value={email}
+              onChange={handleEmailChange}
+              className="w-full lg:min-w-[40rem] border border-ai-blue px-[1.2rem] py-[0.6rem] lg:p-[1.6rem] rounded-[1.2rem] text-[1.4rem] lg:text-[2.1rem] leading-[1.8rem] lg:leading-[2.8rem] font-medium text-ai-blue placeholder:text-[1.4rem] lg:placeholder:text-[2.1rem] placeholder:leading-[1.8rem] lg:placeholder:leading-[2.8rem] placeholder:font-medium placeholder:text-ai-blue focus:outline-ai-blue invalid:border-ai-red invalid:text-ai-red invalid:focus:outline-ai-red"
+            />
+            {status === "error" && (
+              <div
+                className="text-ai-red text-[1.4rem] lg:text-[1.6rem] mb-4 lg:mb-0"
+                dangerouslySetInnerHTML={{
+                  __html:
+                    message?.toString() ||
+                    "Something went wrong. Please try again later.",
+                }}
+              />
+            )}
+          </div>
+        )}
+
         {status === "sending" && (
-          <div className="mb-[0.8rem] lg:mb-0 lg:mr-[2.4rem]">
+          <div className="w-full flex flex-col justify-center lg:justify-start items-center">
             <LeapFrog size={40} speed={2} color="#0000ff" />
           </div>
         )}
 
         {status === "success" && (
-          <div className="font-sans lg:font-inter font-bold text-[1.8rem] lg:text-[2.4rem] leading-[2.3rem] lg:leading-[2.9rem] text-ai-blue mb-[0.8rem] lg:mb-0 lg:mr-[2.4rem] flex items-center grow">
+          <div className="font-sans lg:font-inter font-bold text-[1.8rem] lg:text-[2.4rem] leading-[2.3rem] lg:leading-[2.9rem] text-ai-blue flex items-center grow">
             <p
               dangerouslySetInnerHTML={{
-                __html: message?.toString() || "Thank you for signing up.",
+                __html:
+                  message?.toString() ||
+                  "Thank you for signing up. Thank you for signing up. Thank you for signing up. Thank you for signing up. Thank you for signing up. Thank you for signing up. Thank you for signing up. Thank you for signing up. Thank you for signing up. Thank you for signing up.",
               }}
             ></p>
           </div>
         )}
-        <div className="w-full flex flex-col">
-          {(status === null || status === "error") && (
-            <div className="grow mb-[0.8rem] lg:mb-0 lg:mr-[2.4rem]">
-              <input
-                aria-label="Your email"
-                ref={(node) => (emailInputElement = node)}
-                type="email"
-                placeholder="Your email"
-                value={email}
-                onChange={handleEmailChange}
-                className="w-full lg:min-w-[40rem] border border-ai-blue px-[1.2rem] py-[0.6rem] lg:p-[1.6rem] rounded-[1.2rem] text-[1.4rem] lg:text-[2.1rem] leading-[1.8rem] lg:leading-[2.8rem] font-medium text-ai-blue placeholder:text-[1.4rem] lg:placeholder:text-[2.1rem] placeholder:leading-[1.8rem] lg:placeholder:leading-[2.8rem] placeholder:font-medium placeholder:text-ai-blue focus:outline-ai-blue invalid:border-ai-red invalid:text-ai-red invalid:focus:outline-ai-red"
-              />
-            </div>
-          )}
-          {status === "error" && (
-            <div
-              className="text-ai-red text-[1.4rem] lg:text-[1.6rem] mb-4 lg:mb-0"
-              dangerouslySetInnerHTML={{
-                __html:
-                  message?.toString() ||
-                  "Something went wrong. Please try again later.",
-              }}
-            />
-          )}
-        </div>
 
         <div className="flex flex-col items-center">
           <JoinWhitelistButton
@@ -101,12 +98,14 @@ const CustomForm = ({ status, message, onValidated }) => {
             className="grow-0 shrink-0"
           />
           {/* TODO: Add a deep link */}
-          <a
-            href="#!"
-            className="font-inter font-medium text-[1.4rem] lg:text-[2.1rem] leading-[1.8rem] lg:leading-[2.8rem] text-ai-blue hover:text-ai-blue underline mt-[1.6rem]"
-          >
-            Already a MailTime user
-          </a>
+          {status !== "success" && (
+            <a
+              href="#!"
+              className="font-inter font-medium text-[1.4rem] lg:text-[2.1rem] leading-[1.8rem] lg:leading-[2.8rem] text-ai-blue hover:text-ai-blue underline mt-[1.6rem]"
+            >
+              Already a MailTime user
+            </a>
+          )}
         </div>
       </div>
     </section>

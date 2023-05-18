ee708d4def9c56c0d61697dc219c6945a05478ad
fontofonto <donlam1994@gmail.com>
2023-05-18 03:01:39 -0700

    add speech bubbles animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import LogoSpeechBubbles from "@/components/LogoSpeechBubbles.js";
 
 const Navbar = () => (
   <nav>
     <div className="w-full lg:w-[70%] 2xl:w-[100rem] mx-auto px-[2.5rem] lg:px-0 pt-[2rem] pb-[1.3rem] lg:py-[3.6rem]">
       <div
-        className="w-[17rem] h-[3rem] lg:w-[22.8rem] lg:h-[4rem]"
+        className="w-[17rem] h-[3rem] lg:w-[22.8rem] lg:h-[4rem] relative"
         id="mailtime-logo"
       >
         <a
@@ -21,6 +22,7 @@ const Navbar = () => (
             priority
           />
         </a>
+        <LogoSpeechBubbles />
       </div>
     </div>
   </nav>

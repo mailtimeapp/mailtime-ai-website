a303c269ae3cd746c463ed748c2f387f43a0f166
fontofonto <donlam1994@gmail.com>
2023-08-15 17:32:50 +0800

    Update the layout of the AccordionItem component and add icons

--- src/components/Faq/AccordionItem.js
+++ src/components/Faq/AccordionItem.js
@@ -1,17 +1,38 @@
+import Image from "next/image";
 import { useState } from "react";
+import IconExpandMore from "../Icon/ExpandMore";
 
-const FaqAccordionItem = ({
-  children,
-  title,
-  onClickHandler: onClick,
-  ...props
-}) => {
+const FaqAccordionItem = ({ children, icon, title, onClick, ...props }) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <div {...props}>
-      <button onClick={() => onClickHandler()} className="bg-[#F1F5F9]">
-        {title}
+      <button
+        onClick={() => setIsActive(!isActive)}
+        className={`flex flex-row items-center space-x-[1.6rem] rounded-[2.4rem]  p-[1.6rem] text-[1.4rem] font-bold transition ${
+          isActive ? "bg-[#334155] text-white" : "bg-[#F1F5F9] text-[#334155]"
+        }`}
+      >
+        <span>
+          <Image
+            src={`/static/img/faq/faq_icon-${icon}${
+              isActive ? "-inverted" : ""
+            }.svg`}
+            alt=""
+            width={24}
+            height={24}
+          />
+        </span>
+        <span>{title}</span>
+        <span>
+          <IconExpandMore
+            className={`transform transition ${
+              isActive
+                ? "-scale-y-100 fill-current text-white"
+                : "scale-y-100 fill-current text-[#ADB3BB]"
+            }`}
+          />
+        </span>
       </button>
       {isActive && <div>{children}</div>}
     </div>
--- src/pages/app/faq.js
+++ src/pages/app/faq.js
@@ -5,7 +5,28 @@ const Faq = () => {
   return (
     <div>
       <FaqAccordion>
-        <FaqAccordionItem title="What is MailTime AI?">
+        <FaqAccordionItem title="What is MailTime AI?" icon="mailtime_ai">
+          <div>content</div>
+        </FaqAccordionItem>
+        <FaqAccordionItem title="How does MailTime AI work?" icon="mailtime_ai">
+          <div>content</div>
+        </FaqAccordionItem>
+        <FaqAccordionItem
+          title="What is MailTime AI Unlimited?"
+          icon="mailtime_ai_unlimited"
+        >
+          <div>content</div>
+        </FaqAccordionItem>
+        <FaqAccordionItem title="What is Measurable Data Token?" icon="mdt">
+          <div>content</div>
+        </FaqAccordionItem>
+        <FaqAccordionItem
+          title="How to unsubscribe from MailTime AI Unlimited?"
+          icon="mailtime_ai_unlimited"
+        >
+          <div>content</div>
+        </FaqAccordionItem>
+        <FaqAccordionItem title="What is RewardMe?" icon="rewardme">
           <div>content</div>
         </FaqAccordionItem>
       </FaqAccordion>

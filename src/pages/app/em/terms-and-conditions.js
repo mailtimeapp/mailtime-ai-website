01dff0e9638ab29bc92f1280f8d73c54d59ed1b4
fontofonto <donlam1994@gmail.com>
2024-09-23 01:07:31 -0700

    Duplicate FAQ and Terms pages for Email Messenger

---
+++ src/pages/app/em/terms-and-conditions.js
@@ -0,0 +1,238 @@
+const TermsAndConditionsPage = () => {
+  return (
+    <div className="px-[1.6rem] pb-[2.4rem] pt-[0.8rem]">
+      <div className="mx-auto max-w-[60rem]">
+        <article className="text-[1.4rem] leading-relaxed text-[#334155]">
+          <h2 className="my-8 underline underline-offset-2">
+            <b>Measurable Data Token (MDT)</b>
+          </h2>
+          <p className="my-8">
+            The <b>MDT</b> provided during the priority access phase is designed
+            to be restricted and managed in a centralised way.{" "}
+            <b>It is intended solely for use within the MailTime AI system.</b>{" "}
+            This provision does not constitute a financial investment, security,
+            share, equity, or any form of financial or investment instrument.The
+            tokens are explicitly for use within the confines of the{" "}
+            <b>MailTime AI</b> functionalities, as a means to enhance the user
+            experience and facilitate user participation within the MailTime AI
+            system.
+          </p>
+          <p className="my-8">
+            <b>
+              It can not be withdrawn, redeemed, transferred, or used in any
+              other use cases of the MDT at any point.
+              <br /> Any MDT balance in the user’s Email Messenger accounts can
+              be reclaimed by Measurable Foundation Limited and its affiliates,
+              at its discretion.
+            </b>
+          </p>
+          <p className="my-8">
+            All users should conduct their own due diligence before signing up
+            for MailTime AI whitelisting and subsequently accepting the provided
+            MDT. By receiving the MDT, users agree to be bound by all the
+            restrictions outlined above. Users are solely responsible for
+            understanding and complying with all laws, rules, and regulations of
+            their specific jurisdiction that may be applicable to them in
+            connection with their acceptance and use of MDT.
+          </p>
+
+          <h2 className="my-8 underline underline-offset-2">
+            <b>MailTime AI Credits</b>
+          </h2>
+
+          <p className="my-8">
+            These <b>MailTime AI</b> Terms apply to your access and use of any
+            MailTime AI feature(s) and, if applicable, your{" "}
+            <b>MailTime AI Unlimited</b> Subscription. &ldquo;MailTime AI&rdquo;
+            refers to any feature or functionality provided by Email Messenger
+            and labeled as MailTime AI that utilises data models trained by
+            machine learning.
+          </p>
+
+          <p className="my-8">
+            The MailTime AI Credits are intended solely for use within the
+            MailTime AI system to enhance the user experience and facilitate
+            participation.
+            <b>
+              To maintain optimal performance and fair usage across all MailTime
+              AI users, your usage of MailTime AI is subject to fair usage
+              restrictions. If you exceed the permitted usage, your access to AI
+              features may be reduced.
+            </b>{" "}
+            Please note that all in-app MailTime AI Credit purchases made
+            through Apple/ Google payment systems are final and non-refundable.
+          </p>
+
+          <p className="mb-3 mt-8">
+            By using MailTime AI Credits and/or subscription services, you agree
+            to the following terms and conditions:
+          </p>
+          <ol className="ml-12 list-outside">
+            <li className="my-3 list-decimal">
+              Email Messenger reserves the right to change the pricing of its AI
+              credits and subscription pricing at any time without prior notice
+              to the user.
+            </li>
+            <li className="my-3 list-decimal">
+              Any changes to pricing will be effective immediately upon being
+              posted on the Email Messenger website or otherwise communicated to
+              the user.
+            </li>
+            <li className="my-3 list-decimal">
+              Email Messenger may offer promotional pricing or discounts from
+              time to time, and such promotions may have different terms and
+              conditions than those stated here.
+            </li>
+            <li className="my-3 list-decimal">
+              The user is responsible for paying all fees and charges associated
+              with their use of MailTime AI credits and subscription services.
+            </li>
+            <li className="my-3 list-decimal">
+              All fees and charges are non-refundable, except as required by
+              law.
+            </li>
+            <li className="my-3 list-decimal">
+              Email Messenger reserves the right to suspend or cancel a
+              user&apos;s account at any time for any reason, including but not
+              limited to non-payment of fees and charges.
+            </li>
+            <li className="my-3 list-decimal">
+              Email Messenger may modify or discontinue its AI credits and
+              subscription services at any time without prior notice to the
+              user.
+            </li>
+            <li className="my-3 list-decimal">
+              The user agrees to comply with all applicable laws and regulations
+              when using MailTime AI Credits and subscription services.
+            </li>
+            <li className="my-3 list-decimal">
+              All subscriptions purchased through MailTime AI are automatically
+              set to renew on a recurring basis unless the user chooses to
+              unsubscribe or terminate the subscription. Cancellation of
+              subscriptions must be initiated through the designated channels
+              provided by Google Play and the App Store, and the terms and
+              conditions of these platforms apply to such cancellations. Users
+              are advised to review and comply with the specific terms and
+              conditions outlined by Google Play and the App Store when managing
+              their subscription status.
+            </li>
+            <li className="my-3 list-decimal">
+              If a user chooses to purchase credits on RewardMe but later
+              decides to subscribe to a monthly or yearly MailTime AI unlimited
+              plan, the subscription will take precedence over the credit
+              balance. The existing credits will not be consumed or deducted
+              from the user&apos;s account until the subscription is canceled.
+              If the subscription is canceled, the remaining credits will become
+              available for the user to utilise again.
+            </li>
+            <li className="my-3 list-decimal">
+              The user agrees to provide accurate and complete information when
+              registering for MailTime AI Credits and subscription services, and
+              to promptly update such information as necessary.
+            </li>
+            <li className="my-3 list-decimal">
+              Email Messenger may use the user&apos;s personal information in
+              accordance with its Privacy Policy, which is incorporated by
+              reference into these terms and conditions.
+            </li>
+          </ol>
+          <p className="my-8">
+            Please read these terms and conditions carefully before using
+            MailTime AI Credits and subscription services. By using these
+            services, you agree to be bound by these terms and conditions.
+          </p>
+
+          <h2 className="my-8 underline underline-offset-2">
+            <b>RewardMe Exclusive Privileges for MailTime AI</b>
+          </h2>
+
+          <p className="my-8">
+            RewardMe offers different membership tiers, each with its own set of
+            benefits related to MailTime AI.
+          </p>
+          <p className="my-8">
+            Lower-Tier Benefits: Members in lower tiers receive monthly
+            Measurable Data Tokens (MDTs) to enhance their usage of MailTime AI.
+            The specific allocation is detailed below.
+          </p>
+
+          <ol className="ml-12 list-outside">
+            <li className="my-3 list-disc font-bold">
+              Newbie, Starter, Extra Tier - 100 MDTs, monthly refresh.
+            </li>
+          </ol>
+
+          <p className="my-8">
+            Higher-Tier Benefits: Members in higher tiers enjoy unlimited
+            access* to MailTime AI, as long as they maintain their respective
+            tier status. <br />
+            <b>This includes: Elite, Infinite and Infinite Privilege Tiers.</b>
+          </p>
+
+          <p className="my-8">
+            For users who purchased a subscription and then claimed their
+            RewardMe Exclusive Privileges for MailTime AI (applicable for Elite,
+            Infinite and Infinite Privilege Tiers), you agree to the following
+            terms and conditions:
+          </p>
+
+          <ol className="ml-12 list-outside">
+            <li className="my-3 list-decimal">
+              Subscription Renewal: Unless cancelled manually, your purchased
+              subscription will be automatically renewed at the end of each
+              billing cycle. This renewal will occur on top of the existing
+              RewardMe privileges you already have. To avoid unwanted charges,
+              it is your responsibility to cancel/pause the subscription before
+              the renewal date.
+            </li>
+            <li className="my-3 list-decimal">
+              Refunds and Disputes: Refunds or disputes related to the
+              subscription should be addressed directly with the platform or
+              service from which the subscription was purchased. Email
+              Messenger/RewardMe are not responsible for refunding or resolving
+              any issues related to third-party subscriptions.
+            </li>
+            <li className="my-3 list-decimal">
+              Changes to Terms and Conditions: RewardMe reserves the right to
+              modify or update these terms and conditions at any time. Any
+              changes will be communicated to you through appropriate channels.
+              Continued use of the subscription and RewardMe membership after
+              any modifications implies your acceptance of the updated terms and
+              conditions.
+            </li>
+          </ol>
+
+          <p className="my-8">
+            Tiers advancement is subject to the terms and conditions specified
+            in RewardMe. Members are advised to review and comply with these
+            terms and conditions to be eligible for tier progression. RewardMe
+            reserves the right to modify or update these terms and conditions as
+            necessary. By participating in the tier advancement process, members
+            agree to abide by the rules and guidelines set forth in the
+            RewardMe.
+          </p>
+
+          <p className="my-8">
+            <b>
+              *To maintain optimal performance and fair usage across all
+              MailTime AI users, your usage of MailTime AI is subject to fair
+              usage restrictions. If you exceed the permitted usage, your access
+              to AI features may be reduced.
+            </b>
+          </p>
+
+          <p className="my-8">
+            <b>
+              Compliance and Termination: Members must adhere to the terms and
+              conditions of RewardMe and maintain eligibility for their
+              respective tier. Failure to comply may result in the loss of tier
+              benefits or even termination of membership.
+            </b>
+          </p>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default TermsAndConditionsPage;
